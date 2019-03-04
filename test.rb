1c510595f7f45f4da548180091cc3f938ba9bdcb
Sharmina Akhtar <sakht318@gmail.com>
2019-03-03 22:10:43 -0500

    Update wait time

--- test.rb
+++ test.rb
@@ -5,20 +5,20 @@ require 'test/unit'
 
 class BitlyTest < Test::Unit::TestCase
     def test_bitly
-        @browser = Selenium::WebDriver.for :chrome
+        @browser = Selenium::WebDriver.for :safari
         search_page = Bitly.new(@browser)
         search_page.open()
         search_page.search('https://www.youtube.com/watch?v=oK4rRWcm3fM')
-        sleep 5
+        sleep 10
         search_page.close()
     end
 
     def test_bitly_2
-        @browser = Selenium::WebDriver.for :chrome
+        @browser = Selenium::WebDriver.for :safari
         search_page = Bitly.new(@browser)
         search_page.open()
         search_page.search('https://www.pexels.com/search/flower/')
-        sleep 5
+        sleep 10
         search_page.close()
     end
 end
